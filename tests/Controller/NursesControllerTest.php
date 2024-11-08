c0cbd0f236ad09d18380fc909a124ae8be90e5bb
Jonathan <canojonathanbenavides@gmail.com>
2024-11-08 16:01:22 +0100

    Basic test

--- tests/Controller/NursesControllerTest.php
+++ tests/Controller/NursesControllerTest.php
@@ -2,22 +2,68 @@
 
 namespace App\Tests\Controller;
 
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class NursesControllerTest extends WebTestCase
 {
-    public function testHomePage()
+    private KernelBrowser $client;
+
+    protected function setUp(): void
     {
-        // Crear el cliente simulado
-        $client = static::createClient();
+        $this->client = static::createClient();
+    }
 
-        // Realizar una solicitud GET a la página de inicio
-        $crawler = $client->request('GET', '/');
-
-        // Verificar que la respuesta es exitosa (código de estado 200)
+    public function testIndex(): void
+    {
+        $this->client->request('GET', '/nurses');
         $this->assertResponseIsSuccessful();
+        $this->assertJson($this->client->getResponse()->getContent());
+    }
 
-        // Verificar que el contenido de la etiqueta <h1> contiene el texto "Bienvenido"
-        $this->assertSelectorTextContains('h1', 'Bienvenido');
+    public function testNew(): void
+{
+    $this->client->request('POST', '/nurses/new', [
+        'nombre' => 'test_user',
+        'pass' => 'password123',
+    ]);
+
+    $this->assertResponseIsSuccessful();
+    $responseContent = $this->client->getResponse()->getContent();
+    $this->assertJson($responseContent);
+    $this->assertStringContainsString('"Register":"Success"', $responseContent);
+}
+
+
+    public function testShow(): void
+    {
+        // Asumiendo que existe un enfermero con ID 1 para fines de prueba
+        $this->client->request('GET', '/nurses/show/1');
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($this->client->getResponse()->getContent());
+    }
+
+    public function testEdit(): void
+    {
+        // Asumiendo que existe un enfermero con ID 1 para fines de prueba
+        $this->client->request('PUT', '/nurses/edit/1', [], [], [], json_encode([
+            'user' => 'updated_user',
+            'pass' => 'new_password123',
+        ]));
+
+        $this->assertResponseIsSuccessful();
+        $responseContent = $this->client->getResponse()->getContent();
+        $this->assertJson($responseContent);
+        $this->assertStringContainsString('"nurse":"modified"', $responseContent);
+    }
+
+    public function testDelete(): void
+    {
+        // Asumiendo que existe un enfermero con ID 1 para fines de prueba
+        $this->client->request('POST', '/nurses/delete/1');
+        $this->assertResponseIsSuccessful();
+        $responseContent = $this->client->getResponse()->getContent();
+        $this->assertJson($responseContent);
+        $this->assertStringContainsString('"message":"Nurse deleted successfully"', $responseContent);
     }
 }
